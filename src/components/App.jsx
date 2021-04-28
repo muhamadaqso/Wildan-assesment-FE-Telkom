c36fc1d308866431820a5cd9a31ff9d3a3b2eddb
Wildan Muhamad Aqso <wildanaqso198@gmail.com>
2021-04-29 02:57:43 +0700

    add single logo fot navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import singleLogo from '../assets/single-logo.png';
 import React from 'react';
 import { Navbar} from 'react-bootstrap';
 
@@ -7,7 +8,7 @@ class App extends React.Component {
             <div className="main-app">
                 <header className="main-header shadow-sm">
                      <Navbar bg="dark" variant="dark">
-                      <Navbar.Brand href="/" className="mx-auto">- Github Source -</Navbar.Brand>
+                      <Navbar.Brand href="/" className="mx-auto"><img className="mr-2" width="35" src={singleLogo} alt=""/> Github Source </Navbar.Brand>
                     </Navbar>
                 </header>
                 <main className="main-content">
