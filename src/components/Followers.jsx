51b632f8c8356c72242e3ac1211ac9f9df14f2d9
Wildan Muhamad Aqso <wildanaqso198@gmail.com>
2021-04-29 01:36:30 +0700

    fetch and show list followers

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Followers extends React.Component {
     constructor() {
         super();
-        this.state = [];
+        this.state = {};
     }
 
     componentDidMount() {
@@ -19,11 +19,32 @@ class Followers extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                follwers page
+        if (!this.state.followers) {
+            return <div>LOADING FOLLOWERS...</div>
+        }
+            
+            const followers = this.state.followers;
+            const items = [];
+            for (const [index, value] of followers.entries()) {
+                items.push(
+                    <div className="col-md-4 mb-2" key={index}>
+                        <div className="d-flex">
+                            <img src={value.avatar_url} alt=""/>
+                            <p className="ml-2">{value.login}</p>
+                        </div>
+                    </div>
+                )
+              }
+
+            console.log(followers)
+            return (
+            <div className="followers-page container">
+                <h6>Followers of {this.props.params.username} :</h6>
+                <div className="row mt-3">
+                    {items}
+                </div>
             </div>
-        )
+            );
     }
 }
 
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -31,47 +31,49 @@ class User extends React.Component {
         const user = this.state.user;
 
         return (
-            <div className="container mt-4 user-info">
+            <div className="container mt-4">
+                    <Button variant="primary" href="/"><FontAwesomeIcon icon={faAngleLeft} className="mr-1" />Back</Button>
                 
-                <Button variant="primary" href="/"><FontAwesomeIcon icon={faAngleLeft} className="mr-1" />Back</Button>
-                <hr/>
-                <div className=" text-center mt-3">
-                    <div className="row">
-                        <div className="col-md-6 shadow-sm mx-auto  py-4 ">
-                            <Link className="user-info-profile" to={`/user/${user.login}`}>
-                                <img src={user.avatar_url} alt={`${user.login} avatar`}/>
-                                <div className="my-3">
-                                <span className="px-2 py-1" > <FontAwesomeIcon icon={faUser} className="mr-2" />{user.login}</span>
-                                </div>
-                                <h6 className="mt-2">{user.name}</h6>
-                                <p>{user.bio}</p>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-                <hr/>
-
-                    <div className="row">
-                        <div className="col-4 text-center cursor-pointer">
-                            <p className="mb-1 font-weight-semibold">{user.public_repos}</p>
-                            <p>Repository</p>
-                        </div>
-                        <div className="col-4 text-center cursor-pointer">
-                            <Link  to={`/user/${user.login}/followers`}>
-                                <p className="mb-1 font-weight-semibold">{user.followers}</p>
-                                <p>Followers</p>
-                            </Link>
-                        </div>
-                        <div className="col-4 text-center cursor-pointer">
-                            <p className="mb-1 font-weight-semibold">{user.following}</p>
-                            <p>following</p>
-                        </div>
-                    </div>
-
+                <div className="user-info">
                     <hr/>
-                    <div>
-                        {this.props.children}
+                    <div className=" text-center mt-3">
+                        <div className="row">
+                            <div className="col-md-6 shadow-sm mx-auto  py-4 ">
+                                <Link className="user-info-profile" to={`/user/${user.login}`}>
+                                    <img src={user.avatar_url} alt={`${user.login} avatar`}/>
+                                    <div className="my-3">
+                                    <span className="px-2 py-1" > <FontAwesomeIcon icon={faUser} className="mr-2" />{user.login}</span>
+                                    </div>
+                                    <h6 className="mt-2">{user.name}</h6>
+                                    <p>{user.bio}</p>
+                                </Link>
+                            </div>
+                        </div>
                     </div>
+                    <hr/>
+
+                        <div className="row">
+                            <div className="col-4 text-center cursor-pointer">
+                                <p className="mb-1 font-weight-semibold">{user.public_repos}</p>
+                                <p>Repository</p>
+                            </div>
+                            <div className="col-4 text-center cursor-pointer">
+                                <Link  to={`/user/${user.login}/followers`}>
+                                    <p className="mb-1 font-weight-semibold">{user.followers}</p>
+                                    <p>Followers</p>
+                                </Link>
+                            </div>
+                            <div className="col-4 text-center cursor-pointer">
+                                <p className="mb-1 font-weight-semibold">{user.following}</p>
+                                <p>following</p>
+                            </div>
+                        </div>
+
+                        <hr/>
+                        <div>
+                            {this.props.children}
+                        </div>
+                </div>
             </div>
         );
     }
